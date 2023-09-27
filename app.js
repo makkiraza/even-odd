fd428a6998efcbfe5acd606b9e72e4a56bb2a6dc
makkiraza <makkirazaa@gmail.com>
2023-09-28 03:29:35 +0500

    hogya

---
+++ app.js
@@ -0,0 +1,38 @@
+// var abc = +prompt ('Enter your Value')
+
+// if{
+//     ( abc 2%=== 0 (
+//         Number id odd
+//     ) )
+// }
+// else{
+//     ('odd number')
+// }
+
+
+var userinput = +prompt ('Enter your Percentage')
+
+if (userinput === 100) {
+    document.write('A+')
+}
+
+if (userinput == 80) {
+    document.write('Cha gaye')
+}
+
+if (userinput == 70) {
+    document.write('Buhut Ache')
+}
+
+if (userinput == 60) {
+    document.write('khair hai koshish jari rakhen')
+}
+
+if (userinput == 50) {
+    document.write('sharam ani chaiye maa baap ke paise ziada mat karo')
+}
+
+if (userinput == 40) {
+    document.write('fail')
+}
+
